75c929795001a71e4a168186b0df98304a27fca2
Hans Schroeder <hajschroeder@gmail.com>
2022-03-24 09:40:37 -0700

    include fully functional webpack environment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import './css/styles.css';
 
-const test = [1,2,3,4,5,666];
+const test = [1,2,3,4,5,"telephone"];
 console.log(test);
 function testFunct(){
   let myVar="car";
